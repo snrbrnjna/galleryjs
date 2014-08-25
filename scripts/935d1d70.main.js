29a308272f464edd11796f9b20a71aed557b4cc1
snrbrnjna <snr@brnjna.net>
2014-08-26 01:19:44 +0200

    pushed v0.9.0 to gh pages

---
+++ scripts/935d1d70.main.js
@@ -0,0 +1,30 @@
+/* global define, GalleryApp */
+
+(function () {
+
+  if (typeof define === 'function' && define.amd) { // grunt dev task
+    // if require.js is loaded (=> define is a amd function), 
+    // then load its config and the Gallery Module as such...
+    require(['rconfig', 'gallery/app'], function(nope, GalleryApp) {
+      main(GalleryApp);
+    });
+  } else { // grunt dist task
+    // require.js seems not to be present => GalleryApp has to be loaded as an 
+    // independent script => instanciate it without loading any dependencies
+    main(GalleryApp);
+  }
+
+  function main(GalleryAppClass) {
+    $(function () {
+      // initialize Gallery
+      new GalleryAppClass({
+        el: $('.gallery-app'),
+        galleryOpts: {},
+        onInit: function(app, model) {
+          console.log('Gallery initialized!', app, model);
+        }
+      });
+    });
+  }
+
+})();
